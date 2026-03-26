5105e500e8da1f2e5ab8f05fb5b6b0a51f2e2a62
Keerthivasan Jayakumar <keerthivasanjayakumar@gmail.com>
2026-03-26 11:37:07 +0530

    Updated changes

--- script.js
+++ script.js
@@ -273,3 +273,12 @@ window.addEventListener('scroll', () => {
         }
     });
 });
+
+window.onscroll = function() { updateProgressBar() };
+
+function updateProgressBar() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
